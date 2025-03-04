d7a1f98f7cb4e10dffa2540f26a9fdf43b8f1a07
MathildeBoo <mathildeb497@hotmail.com>
2025-03-04 09:25:35 +0100

    multiplication done

--- src/calculator.py
+++ src/calculator.py
@@ -2,3 +2,5 @@ class Calculator :
     # return the sum between 2 operands
     def mysum(self, first_operand, second_operand):
         return first_operand + second_operand
+    def mymulti(self,first_operand, second_operand):
+         return first_operand * second_operand
--- src/main.py
+++ src/main.py
@@ -13,5 +13,7 @@ if __name__ == '__main__':
 
     if args.operation == 'sum':
         print(f'{args.first_value} + {args.second_value} = {calc.mysum(args.first_value, args.second_value)}')
+    if args.operation == 'multi':
+        print(f'{args.first_value} * {args.second_value} = {calc.mymulti(args.first_value, args.second_value)}')
     else :
         print('The function is not taken into account in our calculator')
