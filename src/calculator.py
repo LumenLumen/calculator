b737a60d42ae13efaf01753eb0747285e9430344
MathildeBoo <mathildeb497@hotmail.com>
2025-03-14 11:40:50 +0100

    message

--- src/calculator.py
+++ src/calculator.py
@@ -2,14 +2,12 @@ class Calculator :
     # return the sum between 2 operands
     def mysum(self, first_operand, second_operand):
         return first_operand + second_operand
-<<<<<<< HEAD
 
     def mymean(self, operands):
         sum = 0
         for nb in operands :
             sum = sum + nb
         return sum / len(operands)
-=======
     def mymulti(self,first_operand, second_operand):
          return first_operand * second_operand
->>>>>>> master
+#commentaire
